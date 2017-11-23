1082cf703e045e2fab5957a36a35a6ded578a888
Walter Weinmann <walter.weinmann@gmail.com>
2017-11-23 20:16:09 +0100

    Adding shell scripts.
    Former-commit-id: 4d9c0f5e3257d0c11fcabb1e12d390a828b434d9

--- check_grammar.bat
+++ check_grammar.bat
@@ -1,25 +1,25 @@
 @ECHO OFF
-rem ----------------------------------------------------------------------------
-rem
-rem check_grammar.bat: SQL - checking grammar definition with BNFC.
-rem
-rem Copyright (c) 2012-17 K2 Informatics GmbH.  All Rights Reserved.
-rem
-rem This file is provided to you under the Apache License,
-rem Version 2.0 (the "License"); you may not use this file
-rem except in compliance with the License.  You may obtain
-rem a copy of the License at
-rem
-rem   http://www.apache.org/licenses/LICENSE-2.0
-rem
-rem Unless required by applicable law or agreed to in writing,
-rem software distributed under the License is distributed on an
-rem "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-rem KIND, either express or implied.  See the License for the
-rem specific language governing permissions and limitations
-rem under the License.
-rem
-rem ----------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
+REM
+REM check_grammar.bat: SQL - checking grammar definition with BNFC.
+REM
+REM Copyright (c) 2012-17 K2 Informatics GmbH.  All Rights Reserved.
+REM
+REM This file is provided to you under the Apache License,
+REM Version 2.0 (the "License"); you may not use this file
+REM except in compliance with the License.  You may obtain
+REM a copy of the License at
+REM
+REM   http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing,
+REM software distributed under the License is distributed on an
+REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM KIND, either express or implied.  See the License for the
+REM specific language governing permissions and limitations
+REM under the License.
+REM
+REM ----------------------------------------------------------------------------
 
 > check_grammar.log (
 
@@ -27,7 +27,9 @@ rem ----------------------------------------------------------------------------
     ECHO !TIME! Start run
     ECHO ----------------------------------------------------------------------------
 
-    DEL /Q tmp\*
+    IF EXIST tmp\* (
+        DEL /Q tmp\*
+    )
 
     bnfc -o tmp --haskell priv\bnf_converter\sqlparse.cf
 
---
+++ check_grammar.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+exec > >(tee -i check_grammar.log)
+sleep .1
+
+# ----------------------------------------------------------------------------
+#
+# check_grammar.bat: SQL - checking grammar definition with BNFC.
+#
+# Copyright (c) 2012-17 K2 Informatics GmbH.  All Rights Reserved.
+#
+# This file is provided to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License.  You may obtain
+# a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# ----------------------------------------------------------------------------
+
+timestamp() {
+  date +"%T"
+}
+
+echo "============================================================================"
+echo "$(timestamp) Start run"
+echo "----------------------------------------------------------------------------"
+
+if [ -d "tmp" ]; then
+   rm tmp/*
+fi
+
+bnfc -o tmp --haskell priv/bnf_converter/sqlparse.cf
+
+happy -i tmp/ParSqlparse.y
+
+echo "----------------------------------------------------------------------------"
+echo "$(timestamp) End   run"
+echo "============================================================================"
+
+exit 0
